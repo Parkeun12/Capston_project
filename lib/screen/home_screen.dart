6764cf0b47a79c5848014d2a342a8ed1c70aeae0
Parkeun12 <qkrdmsco1223@naver.com>
2023-05-09 04:01:59 +0900

    게시글작성페이지 1-2

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -19,12 +19,53 @@ class MainScreen extends StatelessWidget {
         title: Text('C언어 스터디'),
 
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: null),
         actions: [
-          IconButton(icon: Icon(Icons.home), onPressed: null),
+          IconButton(
+              icon: Icon(Icons.home),
+              onPressed: (){
+                print('object');
+              }
+          ),
         ],
       ),
-      body: Text('Home Screen'),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar( //프로필사진
+                //backgroundImage로 이미지 삽입가능
+                backgroundColor: WHITE_COLOR,
+              ),
+              accountName: Text('이름'),
+              accountEmail: Text('email@naver.com'),
+            ),
+            ListTile(
+              title: Text(
+                  '멤버',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: BLUE_COLOR,
+              ),
+              title: Text('이름'),
+              //trailing: Text('내보내기'), 관리자설정에서 내보낼 수 있게
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: DARK_GREY_COLOR,
+              ),
+              title: Text('이름2'),
+              //trailing: Text('내보내기'), 관리자설정에서 내보낼 수 있게
+            )
+          ],
+        ),
+      ),
     );
   }
 }
